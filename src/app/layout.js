be9bd9b0187020463e348f6ca3f84fda560abedd
AbeerRashid <n01781320@humber.ca>
2026-04-04 17:27:56 -0400

    Add Navbar component and update layout structure to include it

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,21 @@
 import { CurrencyProvider } from '../context/CurrencyContext';
+import { Navbar } from '../components/Navbar';
 import "./globals.css";
 
+export const metadata = {
+  title: 'Meridian - Real-time Global Exchange',
+  description: 'Real-time Global Currency Exchange.',
+}
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
        {/* We wrap the children in the Provider so the UI can see the API data and loading state `rates` and `loading` */}
         <CurrencyProvider>
-          {children}
+        <Navbar />
+          <main>
+            {children}
+          </main>
         </CurrencyProvider>
       </body>
     </html>
