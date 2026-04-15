38dc4972bd1277a6c57d406859479bc721cd0c69
Autumn Gun-Munro <autumnleafgunmunro@gmail.com>
2026-04-15 19:34:22 -0400

    Footer names linked to our github profiles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,15 @@ import styles from "./footer.module.css";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      Meridian • Group 4 • Autumn & Abeer • CPAN 144 • 2026
+      Meridian{" • "}Group 4{" • "}
+      <a href="https://github.com/bloodleafpuddle" target="_blank" rel="noopener noreferrer">
+        Autumn
+      </a>
+      {" & "}
+      <a href="https://github.com/AbeerRashid" target="_blank" rel="noopener noreferrer">
+        Abeer
+      </a>
+      {" • "}CPAN 144{" • "}2026
     </footer>
   );
 }
